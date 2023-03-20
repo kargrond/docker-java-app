b5ea3d2e9736011e6b8fb3615ffcc76e0add4242
kargrond <karoline.grondal@inmeta.no>
2023-03-20 14:00:38 +0100

    first commit

---
+++ src/test/java/javacourse/dockerjavaapp/DockerJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package javacourse.dockerjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
